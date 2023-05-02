4dcbb86f34187b81cfa51c21e149e012ee5af7b9
devjean <jeancarloleall@gmail.com>
2023-05-02 01:36:21 -0300

    api-backend

--- src/hooks/useIdentityMutation.ts
+++ src/hooks/useIdentityMutation.ts
@@ -1,5 +1,13 @@
 import { useMutation } from "@tanstack/react-query";
+import { PersonData } from "../interfaces/person-data";
+import axios from "axios";
+
+const postData = (data: PersonData) => {
+  return axios.post("http://localhost:8080/send-data", data);
+};
 
 export function useIdentityMutation() {
-  const = useMutation()
+  const {} = useMutation({
+    mutationFn: (data: PersonData) => postData(data),
+  });
 }
